f656e418650bbce69c6237dcefbc432d1384f347
Star <195926135+Star123451@users.noreply.github.com>
2026-02-17 10:31:49 -0600

    Enhance installation script with command examples
    Added additional commands for installation and configuration instructions.

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-echo "Please try running the Enter the Wired script and the Millennium script pinned in the Linux Support channel at the LuaTools Discord Server."
+echo "Please try running the Enter the Wired script and the Millennium script pinned in the Linux Support channel at the LuaTools Discord Server.
+Or just use these commands right here instead of going there:
+Millennium: curl -fsSL "https://steambrew.app/install.sh" | bash
+SLSsteam + ACCELA: curl -fsSL https://raw.githubusercontent.com/ciscosweater/enter-the-wired/main/enter-the-wired | bash
+LuaTools Plugin: curl -fsSL https://raw.githubusercontent.com/Star123451/LuaToolsLinux/main/update.sh | bash
+REMEMBER TO CONFiGURE ACCELA! CHECK THE PINNED MESSAGE IN THE LINUX SUPPORT CHANNEL FOR THE CORRECT CONFIG!
+Also, set your ACCELA PATH in LuaTools Settings."
